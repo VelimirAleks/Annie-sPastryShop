7328bb0e0e3f6e9f4abe2b5bd9dfc7a705491f7b
Velimir <denator@abv.bg>
2024-03-08 09:40:04 +0200

    Add project files.

---
+++ Annie'sPastryShop/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Annie_sPastryShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
