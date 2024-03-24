698694f89917a63dc8da531c228f5fe95067a843
Velimir <denator@abv.bg>
2024-03-24 17:13:48 +0200

    Change privacy action to about us

--- Annie'sPastryShop/Controllers/HomeController.cs
+++ Annie'sPastryShop/Controllers/HomeController.cs
@@ -18,7 +18,7 @@ namespace Annie_sPastryShop.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
+        public IActionResult AboutUs()
         {
             return View();
         }
