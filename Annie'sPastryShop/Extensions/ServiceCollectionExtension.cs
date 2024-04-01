478a58ae7c531a545371cf383cba4eba0c43065d
Velimir <denator@abv.bg>
2024-04-01 17:57:47 +0300

    Add RequireConfirmedEmail

--- Annie'sPastryShop/Extensions/ServiceCollectionExtension.cs
+++ Annie'sPastryShop/Extensions/ServiceCollectionExtension.cs
@@ -32,6 +32,7 @@ namespace Microsoft.Extensions.DependencyInjection
             services.AddDefaultIdentity<IdentityUser>(options =>
             {
                 options.SignIn.RequireConfirmedAccount = false;
+                options.SignIn.RequireConfirmedEmail = false;
                 options.Password.RequireDigit = false;
                 options.Password.RequireLowercase = false;
                 options.Password.RequireUppercase = false;
