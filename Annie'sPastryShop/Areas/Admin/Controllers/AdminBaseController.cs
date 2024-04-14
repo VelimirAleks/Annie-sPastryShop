a017e11878022eb361d934fa7e213e070e1e3707
Velimir <denator@abv.bg>
2024-04-14 16:59:49 +0300

    Create Admin Area , make some style changes.

---
+++ Annie'sPastryShop/Areas/Admin/Controllers/AdminBaseController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using static AnniesPastryShop.Infrastructure.Constants.DataConstants;
+
+namespace Annie_sPastryShop.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = AdministratorRoleName)]
+    public class AdminBaseController : Controller
+    {
+
+    }
+}
---
+++ Annie'sPastryShop/Areas/Admin/Controllers/MainController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Annie_sPastryShop.Areas.Admin.Controllers
+{
+    public class MainController : AdminBaseController
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
